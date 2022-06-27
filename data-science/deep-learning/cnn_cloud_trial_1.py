d89f55648e71c1e5b30cc21bc49033248f57db97
Dr. Benjamin Manning <benjaminbmanning@gmail.com>
2022-06-27 18:42:13 -0400

    Update cnn_cloud_trial_1.py

--- data-science/deep-learning/cnn_cloud_trial_1.py
+++ data-science/deep-learning/cnn_cloud_trial_1.py
@@ -6,7 +6,9 @@ from keras.layers import Flatten
 from keras.layers import Dense
 import numpy as np
 from keras.preprocessing import image
-# Initialising the CNN
+
+# Part 1 - Initialising the CNN
+
 classifier = Sequential()
 # Step 1 - Convolution
 classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
@@ -22,13 +24,16 @@ classifier.add(Dense(units = 128, activation = 'relu'))
 classifier.add(Dense(units = 1, activation = 'sigmoid'))
 # Compiling the CNN
 classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
 # Part 2 - Fitting the CNN to the images
+
 from keras.preprocessing.image import ImageDataGenerator
 train_datagen = ImageDataGenerator(rescale = 1./255,
 shear_range = 0.2,
 zoom_range = 0.2,
 horizontal_flip = True)
 
+#model setup
 train_datagen = ImageDataGenerator(rescale = 1./255,
 shear_range = 0.2,
 zoom_range = 0.2,
@@ -43,11 +48,13 @@ target_size = (64, 64),
 batch_size = 32,
 class_mode = 'binary')
 
+#training setup
 classifier.fit_generator(training_set,
 steps_per_epoch = 200,
 epochs = 1,
 validation_data = test_set,
 validation_steps = 200)
+
 # Part 3 - Making new predictions
 
 test_image = image.load_img('data/cnn/Cloud_Data/single_prediction/culum_or_strat.jpg', target_size = (64, 64))
@@ -56,11 +63,13 @@ test_image = np.expand_dims(test_image, axis = 0)
 result = classifier.predict(test_image)
 training_set.class_indices
 print(training_set.class_indices)
-#if result[0][0] == 1:
-#	prediction = 'stratus'
-#elif result[0][0] == 2:
-#	prediction = 'culum'
-#	print(prediction)
-#else:
-#	print('The cloud type was not found')
+
+#validate results
+if result[0][0] == 1:
+    prediction = 'stratus'
+elif result[0][0] == 2:
+    prediction = 'culum'
+    print(prediction)
+else:
+    print('The cloud type was not found')
 
